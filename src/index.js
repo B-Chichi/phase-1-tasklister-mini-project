d9078fc49b7fc784167de604942d7cae3377707e
Brian <brian2000bett@gmail.com>
2025-03-16 22:50:59 +0300

    tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const button = document.querySelector("submit");
+  const newTaskForm = document.getElementById("create-task-form");
+
+  document.addEventListener("click", () => {
+    console.log("Clicked");
+
+    const taskList = [];
+
+    const newTaskDescription = document.getElementById(
+      "new-task-description"
+    ).value;
+    console.log(newTaskDescription);
+    taskList.push(newTaskDescription);
+    console.log(taskList);
+  });
+  newTaskForm.addEventListener("click", (value) => {
+    value.preventDefault();
+  });
 });
