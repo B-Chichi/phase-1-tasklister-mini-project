ec497c5078396ca168eb9311305be5ceb64304f5
Brian <brian2000bett@gmail.com>
2025-03-17 20:25:02 +0300

    tasklister

--- src/index.js
+++ src/index.js
@@ -1,20 +1,47 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const button = document.querySelector("submit");
-  const newTaskForm = document.getElementById("create-task-form");
+//document.addEventListener("DOMContentLoaded", () => {
+//const button = document.querySelector("submit");
+const newTaskForm = document.getElementById("create-task-form");
+const taskList = [];
 
-  document.addEventListener("click", () => {
-    console.log("Clicked");
+newTaskForm.addEventListener("submit", (event) => {
+  //console.log("Submitted");
+  event.preventDefault();
 
-    const taskList = [];
+  const input = document.getElementById("new-task-description");
 
-    const newTaskDescription = document.getElementById(
-      "new-task-description"
-    ).value;
-    console.log(newTaskDescription);
-    taskList.push(newTaskDescription);
-    console.log(taskList);
-  });
-  newTaskForm.addEventListener("click", (value) => {
-    value.preventDefault();
-  });
+  taskList.push(input.value.trim());
+
+  newTaskForm.reset();
+
+  console.log(taskList);
+
+  toDoList(taskList);
 });
+
+function toDoList(todo) {
+  const tasks = document.querySelector("#tasks");
+
+  tasks.innerHTML = "";
+
+  todo.forEach((toDo) => {
+    console.log(toDo);
+
+    const li = document.createElement("li");
+    li.textContent = toDo;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "x";
+
+    deleteButton.addEventListener("click", () => {
+      li.remove();
+
+      taskList.splice(taskList.indexOf(toDo),1)
+
+      //console.log(deletedTasks);
+    });
+
+    li.appendChild(deleteButton);
+
+    tasks.appendChild(li);
+  });
+}
